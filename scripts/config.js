b10b530bd9defe8f73a5d40abcc59e511088984c
bob@hesse-ludvigsen.dk <belgien123>
2017-04-21 10:45:10 +0200

    Initial commit

---
+++ scripts/config.js
@@ -0,0 +1,9 @@
+//globals: equal, responseText, statement, ok, deepEqual, QUnit, module, asyncTest, Util, start, golfStatements, console
+/*jslint bitwise: true, browser: true, plusplus: true, maxerr: 50, indent: 4 */
+function Config() {
+	"use strict";
+}
+Config.endpoint = "https://lrs.fels.dk/xAPI/";
+Config.user = "bob1964";
+Config.password = "Belgien.123";
+Config.actor = { "mbox":"bob1964@sandbox.org", "name":"bob1964" };
